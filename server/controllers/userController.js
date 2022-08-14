a57d4f802ad16193fb74b1c140d30902d20fb989
Ali <asadri721@gmail.com>
2022-08-14 22:50:52 +0430

    Add users models, controllers, routes for sign up

---
+++ server/controllers/userController.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import userModel from '../models/userModel.js';
+
+const secret = 'test';
+
+export const signup = async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  try {
+    const oldUser = await userModel.findOne({ email });
+
+    if (oldUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await userModel.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign({ email: result.email, id: result._id }, secret, {
+      expiresIn: '1h',
+    });
+
+    res.status(201).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' });
+    console.log(error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 import connectDB from './config/db.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,7 +14,9 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
-// app.use(express.urlencoded({ limit: '30mb', extended: true }));
+
+//API Routes
+app.use('/users', userRouter);
 
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
