fbc75bf07c30959a0e77c0186b75d37819528d38
Ali <asadri721@gmail.com>
2022-08-15 06:51:38 +0430

    Config mdbootstrap and toastify

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <ToastContainer />
       <h2>Tour</h2>
     </div>
   );
