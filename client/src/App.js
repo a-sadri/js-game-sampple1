c2c9bce3de9ea0e208afdb6ce49beb55c1e6771a
Ali <asadri721@gmail.com>
2022-08-15 06:58:10 +0430

    Implement react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <ToastContainer />
-      <h2>Tour</h2>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <ToastContainer />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
